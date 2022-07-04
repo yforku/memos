b81bf834b74b0b9f073153f33d3ad282c8901d99
Rahmat M. Samik-Ibrahim <19358416+rms46@users.noreply.github.com>
2022-07-04 13:51:32 +0700

    OSP222 cbkadal

---
+++ assets/scripts/SBU-check.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# VER06: Mon 04 Jul 2022 13:30
+# VER05: Tue 03 May 2022 18:30
+# Adapted from LFS:  © 1999-2022 Gerard Beekmans
+# Modified by C. BinKadal. See file LICENCE-MIT.
+#
+BINUTILS="binutils-2.38"
+TARBALL="$LFS/sources/$BINUTILS.tar.xz"
+[ -f $TARBALL ]            || { echo "$TARBALL file: Not found";        exit 1; }
+[[ "$(whoami)" == "lfs" ]] || { echo 'Run this script with user "LFS"'; exit 2; }
+time {
+    echo "HOSTNAME: $(hostname) -- DATE: $(date)"
+    pushd  $LFS/sources/
+    rm -rf $BINUTILS/
+    echo  "tar..."
+    tar    xf $TALBALL
+    cd     $BINUTILS/
+    mkdir -pv build/
+    cd     build/
+    echo  "configure..."
+    ../configure --prefix=$LFS/tools \
+                 --with-sysroot=$LFS \
+                 --target=$LFS_TGT   \
+                 --disable-nls       \
+                 --disable-werror &> /dev/null
+    echo "make..."
+    make &> /dev/null
+    echo "make install"
+    make install &> /dev/null
+    popd
+    rm -rf $BINUTILS/
+    CPUINFO=$(grep -E 'model +name.+' /proc/cpuinfo | head -1 |\
+       awk '{$1="";$2="";$3="";print}' | sed -e 's/^   //')
+    MYMEM=$(free  --giga|grep ^Mem:|awk  '{print $2}')
+    MYSWAP=$(free --giga|grep ^Swap:|awk '{print $2}')
+    echo "Memory: $MYMEM GB -- Swap: $MYSWAP GB -- MAKEFLAGS: $MAKEFLAGS"
+    echo -n "Standard Build Unit (SBU) for $CPUINFO"
+} 
+
